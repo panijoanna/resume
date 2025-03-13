274490feaf1eae4bdf0af3bac103c4844ae1cf2f
panijoanna <pani.joanna93@gmail.com>
2025-03-13 13:05:40 +0100

    feat: create Sidebar with its styles

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+import Logo from "./Logo";
+import { Icon } from "@iconify/react";
+import styles from "../styles/Sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebarContainer}>
+      <Logo />
+      <nav className={styles.sidebarContainer}>
+        <div className={styles.navItem}>
+          <Link href="/infoboard" className={styles.navLink}>
+            <Icon icon="radix-icons:dashboard" className={styles.navIcon} />
+            Tablica
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
