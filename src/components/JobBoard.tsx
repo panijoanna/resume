7a178e3509c1be75f51e9f62f303987ca2f0992e
panijoanna <pani.joanna93@gmail.com>
2025-03-14 13:20:57 +0100

    feat: create JobBoard component

---
+++ src/components/JobBoard.tsx
@@ -0,0 +1,32 @@
+"use client";
+import styles from "../styles/JobBoard.module.css";
+import StatusBadge from "./StatusBadge";
+
+const JobBoard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Moja tablica</h1>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>wszystkie aplikacje</li>
+          <li className={styles.listItem}>w trakcie rekrutacji</li>
+          <li className={styles.listItem}>odrzucone aplikacje</li>
+          <li className={styles.listItem}>oczekujące aplikacje</li>
+        </ul>
+      </div>
+
+      <div className={styles.labelContainer}></div>
+
+      <div className={styles.statusContainer}>
+        <StatusBadge color="green" />
+        <span className={styles.text}>w trakcie rekrutacji</span>
+        <StatusBadge color="orange" />
+        <span className={styles.text}>oczekujące aplikacje</span>
+        <StatusBadge color="red" />
+        <span className={styles.text}>odrzucone aplikacje</span>
+      </div>
+    </div>
+  );
+};
+
+export default JobBoard;
