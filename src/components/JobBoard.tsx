f3a65767ce79a75fd2095f21521d61f6358a953a
panijoanna <pani.joanna93@gmail.com>
2025-03-20 12:05:59 +0100

    refactor: migrate styles from CSS modules to Tailwind CSS

--- src/components/JobBoard.tsx
+++ src/components/JobBoard.tsx
@@ -1,27 +1,44 @@
 "use client";
-import styles from "../styles/JobBoard.module.css";
+import { Box, Typography, ListItem, List } from "@mui/material";
 import CardFormModal from "./CardFormModal";
 
 const JobBoard = () => {
+  const selectedOption = [
+    " All applications",
+    "Applied",
+    "Interview",
+    "Offer",
+    "Rejected",
+  ];
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <h1 className={styles.heading}>My Job Board</h1>
-        <div className={styles.itemContainer}>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>All applications</li>
-            <li className={styles.listItem}>Applied</li>
-            <li className={styles.listItem}>Interview</li>
-            <li className={styles.listItem}>Offer</li>
-            <li className={styles.listItem}>Rejected</li>
-          </ul>
-          <div className={styles.buttonContainer}>
+    <Box className="flex flex-col m-12 h-screen">
+      <Box className="h-1/10 w-full">
+        <Typography
+          variant="h6"
+          className="text-[#0e0d0d] pb-4 mb-0 border-b border-[#ececec]"
+          fontWeight={700}
+          fontSize={18}
+        >
+          My Job Board
+        </Typography>
+        <Box className="flex gap-2">
+          {selectedOption.map((option, index) => (
+            <List
+              key={index}
+              className="flex gap-5 text-[#6c6c7f] list-none"
+            >
+              <ListItem className="cursor-pointer !pl-0 transition duration-300 ease-in-out font-light hover:text-[#247aff]">
+                {option}
+              </ListItem>
+            </List>
+          ))}
+          <Box className="flex items-center ml-auto">
             <CardFormModal />
-          </div>
-        </div>
-      </div>
-      <div className={styles.labelContainer}></div>
-    </div>
+          </Box>
+        </Box>
+      </Box>
+      <Box className="h-4/5"></Box>
+    </Box>
   );
 };
 
